4f0821027dc3b1af338b9605341529542a497389
sanghyun jeon <sh@relaybrand.com>
2023-08-22 17:46:23 +0900

    init commit

---
+++ src/main/java/com/tkdgus/mvcstudy/MvcstudyApplication.java
@@ -0,0 +1,13 @@
+package com.tkdgus.mvcstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcstudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcstudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tkdgus/mvcstudy/MvcstudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tkdgus.mvcstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcstudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
